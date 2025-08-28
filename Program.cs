6676397a930585675141f424d6467655677ffde9
Max Sultan <maxsultankent@gmail.com>
2025-08-28 11:00:15 -0600

    Finished step: 2

--- Program.cs
+++ Program.cs
@@ -6,3 +6,23 @@ int a = 18;
 int b = 6;
 int c = a - b;
 Console.WriteLine(c);
+
+// Order of operations PEMDAS
+// Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+// 9 - 6 * 2 + 48 / 3 + 12
+// 9 - 12 + 16 + 12
+// -3 + 28
+// 25
+Console.WriteLine(d);
+
+// integer division always produces an integer result
+int e = 7;
+int f = 4;
+int g = 3;
+int h = (e + f) / g; // (7 + 4) / 3 -> 11 / 3 -> 3
+Console.WriteLine(h);
+
