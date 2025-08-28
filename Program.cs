f1e7af3bf64c749d8eb0efd194b2610210aef34e
Max Sultan <maxsultankent@gmail.com>
2025-08-28 10:59:25 -0600

    Finished step: 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Step 1: Integer math
+int a = 18;
+int b = 6;
+int c = a - b;
+Console.WriteLine(c);
